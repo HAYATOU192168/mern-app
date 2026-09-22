1b127420559e62e86688825544bdf43f06173a0b
HAYATOU192168 <essa192168@gmail.com>
2026-09-22 20:02:40 +0400

    Use Render port for server

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get("/api", (req, res)=> {
     res.send("hello world")
 })
 
-app.listen(5000, ()=> console.log("server running on port 5000"))
+app.listen(process.env.PORT || 5000, () => console.log("server is running"));
 
 
 
